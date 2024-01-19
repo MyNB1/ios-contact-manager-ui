27c537c3392ed95789adfe1810393324a579447d
MyNB1 <dishdib61@naver.com>
2024-01-19 16:24:43 +0900

    feat: 테이블뷰 셀 삭제 기능 구현 (#6)

--- ContactManager/ContactManager/Sources/Scene/ContactListScene/Controller/ContactListViewController.swift
+++ ContactManager/ContactManager/Sources/Scene/ContactListScene/Controller/ContactListViewController.swift
@@ -63,6 +63,19 @@ extension ContactListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 70
     }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+        
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            tableView.beginUpdates()
+            contactList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            tableView.endUpdates()
+        }
+    }
 }
 
 // MARK: - UITableViewDataSource
